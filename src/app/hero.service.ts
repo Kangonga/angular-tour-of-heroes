e98db681f36517a21cc214ed94cb997f5b2d630d
Kangonga <guchu413@gmail.com>
2023-02-12 14:14:42 +0300

    feat: Add hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
 
-//import the hero interface
-import { Hero } from './hero';
-//import the mock data
-import { HEROES } from './mock-heroes';
-
 @Injectable({
   providedIn: 'root'
 })
